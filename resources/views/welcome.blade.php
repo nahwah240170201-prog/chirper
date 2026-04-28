51c33cf203ed1d4c1b31824187ba92bad7baa4bb
Nahwah Azimah <nahwah.240170201@mhs.unimal.ac.id>
2026-04-28 22:14:28 +0700

    update welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -58,5 +58,19 @@
 
         </div>
 
+        <div class="max-w-2xl mx-auto">
+            @foreach ($chirps as $chirp)
+                <div class="card bg-base-100 shadow mt-8">
+                    <div class="card-body">
+                        <div>
+                            <div class="font-semibold">{{ $chirp['author'] }}</div>
+                            <div class="mt-1">{{ $chirp['message'] }}</div>
+                            <div class="text-sm text-gray-500 mt-2">{{ $chirp['time'] }}</div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+
     </div>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,3 @@
-<?php
+use App\Http\Controllers\ChirpController;
 
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ChirpController::class, 'index']);
