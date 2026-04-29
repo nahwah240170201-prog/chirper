bd72651cf7f4664527df173bb3955e263034a13a
Nahwah Azimah <nahwah.240170201@mhs.unimal.ac.id>
2026-04-29 11:24:39 +0700

    update chirp controller

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -26,6 +26,15 @@ class ChirpController extends Controller
             ]
         ];
 
-        return view('welcome', ['chirps' => $chirps]);
+        return view('welcome', [
+            'chirps' => $chirps
+        ]);
     }
+
+    public function create(){}
+    public function store(Request $request){}
+    public function show(string $id){}
+    public function edit(string $id){}
+    public function update(Request $request, string $id){}
+    public function destroy(string $id){}
 }
